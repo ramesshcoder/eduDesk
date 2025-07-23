9635ff1d061193db1f0311217b625b39cadbc01e
ramesshcoder <rameshkumarer321@gmail.com>
2025-07-23 13:09:03 +0530

    initia commits

---
+++ eduDesk/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { StudentsComponent } from "./students/students.component";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, StudentsComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'eduDesk';
+}
